c6e5bfea16cac7edd637c21efdad8213efe729c6
Tiago Fontes <tiagorafaelfontes39@gmail.com>
2020-11-30 16:39:20 +0000

    Notebooks fixes for new datalake structure

--- AlertAI/getData.py
+++ AlertAI/getData.py
@@ -11,28 +11,22 @@ import shutil
 import pandas as pd
 
 
-url_sensor1 = "http://cehum.ilch.uminho.pt/datalake/sds011/all/csv"
-url_sensor2 = "http://cehum.ilch.uminho.pt/datalake/bme680/all/csv"
+url = "http://cehum.ilch.uminho.pt/datalake/sensors/all/csv"
 
 
 def getRawData():
     try:
-        r_1 = requests.get(url_sensor1, verify=False,stream=True)
-        r_2 = requests.get(url_sensor2, verify=False,stream=True)
+        r= requests.get(url, verify=False,stream=True)
     except:
         print("Failure!! Please check url from DataLake")
         exit()
         
-    r_1.raw.decode_content = True
-    with open("data/sensor1_raw_all.csv", 'wb') as f1:
-        shutil.copyfileobj(r_1.raw, f1)
-    r_2.raw.decode_content = True    
-    with open("data/sensor2_raw_all.csv", 'wb') as f2:
-        shutil.copyfileobj(r_2.raw, f2)
+    r.raw.decode_content = True
+    with open("data/raw_data.csv", 'wb') as f1:
+        shutil.copyfileobj(r.raw, f1)
     print("Success. Data from both sensors collected")
 
 if __name__ == '__main__':
     getRawData()
-    df1 = pd.read_csv("data/sensor1_raw_all.csv")
-    df2 = pd.read_csv("data/sensor2_raw_all.csv")
-    #print(df2.head())
+    df = pd.read_csv("data/raw_data.csv")
+    #print(df.head())
