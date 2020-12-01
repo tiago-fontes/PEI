c5fa70cb04d5ea7a6aa4b1515ea3e19b9326bb83
coutinho <coutinho.andre@icloud.com>
2020-12-01 18:11:10 +0000

    SimpleTable component (refactoring)

---
+++ ridecare_frontend/src/Components/SimpleTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-center">
+              License Plate
+            </th>
+            <th class="text-center">
+              Status
+            </th>
+          </tr>
+        </thead>
+        <tbody class="text-center">
+          <tr v-for="(car, index) in cars" :key="index">
+            <td>{{ car.numberPlate }}</td>
+            <td>
+              <v-tooltip right>
+                <template v-slot:activator="{ on, attrs }">
+                  <span v-bind="attrs" v-on="on">
+                    <v-icon color="red">mdi-checkbox-blank-circle</v-icon>
+                  </span>
+                </template>
+                <span>Car Offline</span>
+              </v-tooltip>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SimpleTable",
+  props: ["cars"]
+};
+</script>
+
+<style scoped></style>
--- ridecare_frontend/src/views/Dashboard.vue
+++ ridecare_frontend/src/views/Dashboard.vue
@@ -2,10 +2,7 @@
   <div class="dashboard">
     <v-container>
       <v-row>
-        <v-col
-          cols="12"
-          class="text-center text-sm-left text-h6 font-weight-bold"
-        >
+        <v-col cols="12" class="text-center text-sm-left text-h6 font-weight-bold">
           Dashboard
         </v-col>
       </v-row>
@@ -33,35 +30,7 @@
       </v-row>
       <v-row class="mt-12">
         <v-col cols="12" sm="12" md="6" lg="6">
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-                <tr>
-                  <th class="text-center">
-                    License Plate
-                  </th>
-                  <th class="text-center">
-                    Status
-                  </th>
-                </tr>
-              </thead>
-              <tbody class="text-center">
-                <tr v-for="(car, index) in cars" :key="index">
-                  <td>{{ car.numberPlate }}</td>
-                  <td>
-                    <v-tooltip right>
-                      <template v-slot:activator="{ on, attrs }">
-                        <span v-bind="attrs" v-on="on">
-                          <v-icon color="red">mdi-checkbox-blank-circle</v-icon>
-                        </span>
-                      </template>
-                      <span>Car Offline</span>
-                    </v-tooltip>
-                  </td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
+          <SimpleTable :cars="cars" />
         </v-col>
         <v-col class="text-center" cols="12" sm="12" md="6" lg="6">
           Graph
@@ -73,9 +42,14 @@
 
 <script>
 import MainLayoutVue from "../Layouts/MainLayout.vue";
+
+import SimpleTable from "../Components/SimpleTable.vue";
+
 export default {
   name: "Dashboard",
-  components: {},
+  components: {
+    SimpleTable
+  },
   data() {
     return {
       cars: [
