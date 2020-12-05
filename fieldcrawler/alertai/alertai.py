3d27707579a2ae171cef557f83b1562b9876d1f1
Tiago Fontes <tiagorafaelfontes39@gmail.com>
2020-12-05 22:08:26 +0000

    Prepare to unsup classif | Fix minimal erros

--- fieldcrawler/alertai/alertai.py
+++ fieldcrawler/alertai/alertai.py
@@ -1,15 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# This file is responsible for handle with
+# event classification and anomaly detection
+# AlertAI class is going to receive the collected data,
+# collect the features and procedure to classification
+# It is also the sender of events to backend
 
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 import pickle
+import requests
 
 
-FILE_NAME = 'alertai/svm_test.sav'
-DB_HOST = ''
+SUP_MODEL = 'alertai/svm_test.sav'
+UNSUP_MODEL = ''
+DB_HOST = 'localhost:7777'
 
 class AlertAI:
 
@@ -21,9 +26,11 @@ class AlertAI:
 
     def start(self):
         #Iniciar modelos e definir configs
-        self.model = pickle.load(open(FILE_NAME, 'rb'))
+        self.sup_model = pickle.load(open(SUP_MODEL, 'rb'))
+        #INSERT NEXT LINE TO ADD UNSUPERVISED MODEL 
+        #self.unsup_model = pickle.load(open(UNSUP_MODEL 'rb'))
         print("Start went good")
-        #pass
+        
 
     def update(self, dataDict):
         self.sensorsData = dataDict
@@ -32,67 +39,69 @@ class AlertAI:
         self.processData()
 
     def processData(self):
-        #Process / Call and check models
-        #Chamar outras funções
-        #LEMBRAR DE TIRAR A PROXIMA LINHA DE COMENTARIO PARA PROD ENV -------------------------------------------------------------
+        # We must handle with unnecessary columns
         raw_data = self.sensorsData
         raw_data.pop('carId')
         raw_data.pop('carLocation')
         raw_data.pop('timeValue')
         raw_data.pop('tags')
         raw_data.pop('classification')
-        columns = ['id','carId','carLocation','timeValue','pm25','pm10','temperature','gas','humidity','pressure','altitude']
+        columns = ['pm25','pm10','temperature','gas','humidity','pressure','altitude']
         data = pd.DataFrame([raw_data])
-        
-        #data = raw_data[['sensors.pm25','sensors.pm10','sensors.temperature','sensors.gas','sensors.humidity','sensors.pressure','sensors.altitude']]
-        #Normalization with MinMax
+        #Normalization with MinMax Scaler
         minmax = MinMaxScaler()
         minmaxData = pd.DataFrame(minmax.fit_transform(data.values), columns=data.columns, index=data.index)
         #Standarization with StandardScaler
         scaler = StandardScaler()
         standardData = pd.DataFrame(scaler.fit_transform(data.values), columns=data.columns, index=data.index)
         result = [data,minmaxData,standardData]
+        # Classify data
         self.classifyData(result)
-        #return result
-        #etc
 
 
     def classifyData(self,data):
         #Classify
         #Note: DATA[0] -> rawData ; DATA[1] -> MINMAX  ; DATA[2] -> STANDARDSCALER
         data2Use = data[0]
-        classif = self.model.predict(data2Use)
-        # show the inputs and predicted outputs
-        #for i in range(len(data2Use)):
-            #print("Value = %s, Classification = %s" % (data2Use[i], classif[i]))
-        print("Value = %s \n Classification = %s " % (data2Use, classif[0]))
+        classif = self.sup_model.predict(data2Use)
+        classification = classif[0]
+        # show the input and predicted output
+        print("Value = %s \n Classification = %s " % (data2Use, classification))
         print("Classification process done!!")
-        #self.prepare2Send(self.sensorsData, classif)
-        #etc
+        if(classification==1)
+            self.prepare2Send(classification)
+        
 
-    def prepare2Send(self, data, classification):
+    def prepare2Send(self,classification):
         #Prepare data before sending to backend
-        #Arrange in structure defined, according to all variables
-        #Note: argument data represents all the variables collect, i.e., includes id, localization, etc
-        classif = classification[0]
-        arrangedData = data[['sensors.timevalue','sensors.pm25','sensors.pm10','sensors.temperature','sensors.gas','sensors.himidity','sensors.pressure','sensors.altitude']]
-        location = data[['sensors.carlocation']]
+        beforeSend = self.sensorsData
+        beforeSend.pop('carId')
+        beforeSend.pop('timeValue')
+        beforeSend.pop('carLocation')
+        # In PROD environment, next 2 lines deleted
+        beforeSend.pop('tags')
+        beforeSend.pop('classification')
+        arrangedData = data = pd.DataFrame([beforeSend])
         #Split carlocation to get latitude % longitude separately
+        location = self.sensorsData.get('carLocation')
         subStr = location.split(" ")
-        arrangedData['latitude'] = subStr[0]
-        arrangedData['longitude'] = subStr[1]
+        arrangedData['longitude'] = subStr[0]
+        arrangedData['latitude'] = subStr[1]
+        arrangedData['date'] = self.sensorsData.get('timeValue')
         #Add classification value to array of data
         arrangedData['classification'] = classif
-        #return arrangedData
-        self.saveData(arrangedData)
+        #Send data to backend
+        #self.saveData(arrangedData)
 
 
     def saveData(self,arrangedData):
         #Save in db cloud
         #Sendo to endpoint POST
         try:
-            #res = requests.post(DB_HOST, json=arrangedData)
+            headers: {"carID" : self.sensorsData.get('carId')}
+            res = requests.post(DB_HOST, json=arrangedData,headers=headers)
             print("Sent to backend :D ")
+            print(res.text)
         except:
             print("Something went wrong! :( ")
 
