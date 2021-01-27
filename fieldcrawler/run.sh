5a8d1ec33842662aafec570c795ee4c697e5650c
leandrocosta16 <leandro-costa-14@hotmail.com>
2021-01-27 17:39:26 +0000

    run script fix

--- fieldcrawler/run.sh
+++ fieldcrawler/run.sh
@@ -6,11 +6,11 @@ apt install redis-server
 
 pip3 install -r requirements.txt
 
-cp fieldcrawler.service /lib/systemd/sysytem/fieldcrawler.service
-cp pei-worker.service /lib/systemd/sysytem/pei-worker.service
+cp fieldcrawler.service /lib/systemd/system/fieldcrawler.service
+cp pei-worker.service /lib/systemd/system/pei-worker.service
 
-chmod 644 /lib/systemd/sysytem/fieldcrawler.service
-chmod 644 /lib/systemd/sysytem/pei-worker.service
+chmod 644 /lib/systemd/system/fieldcrawler.service
+chmod 644 /lib/systemd/system/pei-worker.service
 
 chmod +x main.py
 chmod +x worker.py
