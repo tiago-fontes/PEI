2438c623387c124bb3818af1a4665b448f00f28f
João Costa <jcosta998@hotmail.com>
2021-01-28 03:04:51 +0000

    Branch Organization | Cloud Cleaning

--- AlertAI/AlertAI_Cloud/cloud_test_Models.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BASE = "http://34.76.239.59:5000/"
-
-
-params = {'licensePlate': "VP-35-44", 'timeValue': "2020-12-11 18:16:24" }
-
-response = requests.get(BASE + "models",params=params)
-print(response.text)
